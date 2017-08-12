61e32bca294e727d4dbb0e275fdf78891673a81f
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2017-08-12 13:40:35 +0100

    Update and rename SlapperXPOrb.php to SlapperXPorb.php

--- src/slapper/entities/other/SlapperXPorb.php
+++ src/slapper/entities/other/SlapperXPorb.php
@@ -1,12 +1,12 @@
 <?php
 namespace slapper\entities\other;
 
-use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\network\mcpe\protocol\AddEntityPacket;
 use pocketmine\Player;
 use slapper\entities\SlapperEntity;
 
 
-class SlapperXPOrb extends SlapperEntity {
+class SlapperXPorb extends SlapperEntity {
 
     public $entityId = 69;
     public $offset = 0.8;
